1e1fdcde7158359805c0aa5d82d25470c8b034ac
Eva Rodriguez <evarodriguez@Evas-MBP.lan>
2022-01-26 13:14:08 -0500

    Deploying6

--- src/Pages/Reload.js
+++ src/Pages/Reload.js
@@ -3,7 +3,8 @@
 export default function Reload() {
     return (
     <div>
-        <h1>HELLO</h1>
+        <h1>HELLO!!</h1>
+        <h1>Please Click on menu to see magic happen!!</h1>
     </div>
     );
 }
